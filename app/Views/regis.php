46ad442811ad098813c12861b6379f84322cbb81
kenanargya <92387421+kenanargya@users.noreply.github.com>
2023-11-09 20:47:26 +0700

    Update regis.php

--- app/Views/regis.php
+++ app/Views/regis.php
@@ -82,7 +82,7 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <a class="navbar-brand" href="#">
             <img src="<?= base_url('assets/img/kemdikbud.png') ?>" alt="Logo" style="max-width: 50px; max-height: 50px;">
-            PPDB Online (Valdo-PBKK A)
+            PPDB Online
         </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
